baec7d96017645e4044041506da4cd65926be7c3
vandy <chadvandewettering@gmail.com>
2020-05-31 12:22:41 -0400

    remove unneeded logging, fix up the enable_logging to properly work

--- script/_lib/mod/zzz_mct_options.lua
+++ script/_lib/mod/zzz_mct_options.lua
@@ -1,15 +1,18 @@
+-- edits the global environment to reenable out()'s
 local function enable_logging(enable)
     if enable then
         __write_output_to_logfile = true
 
         if __logfile_path == "" then
             __logfile_path = "script_log_" .. os.date("%d".."".."%m".."".."%y".."_".."%H".."".."%M") .. ".txt"
+            _G.logfile_path = __logfile_path
         end
     else
         __write_output_to_logfile = false
     end
 end
 
+-- first init - reads the mct_mod object, then the option "enable_logging", and checks its setting - true/false
 local function init(mct)
     local mod = mct:get_mod_by_key("mct_mod")
     local option = mod:get_option_by_key("enable_logging")
@@ -17,32 +20,20 @@ local function init(mct)
     enable_logging(option:get_finalized_setting())
 end
 
+-- MctOptionSettingFinalized is called for each changed option when settings are finalized
 core:add_listener(
     "MctModEnableLoggingChanged",
     "MctOptionSettingFinalized",
     function(context)
-        out("TEST 1")
-        for k,v in pairs(context) do
-            out(tostring(k))
-            out(tostring(v))
-        end
-        local ok, err = pcall(function()
-        out(tostring(context))
-        out(tostring(context:mod()))
-        out(tostring(context:mod():get_key()))
-        out(context:mod():get_key())
-        out(context:option():get_key())
-        end) if not ok then out(err) end
         return context:mod():get_key() == "mct_mod" and context:option():get_key() == "enable_logging"
     end,
     function(context)
-        out("TEST 2")
-        out(tostring(context:setting()))
         enable_logging(context:setting())
     end,
     true
 )
 
+-- MctInitialized is called shortly after the Lua environment is safe to mess with
 core:add_listener(
     "MctModInitialized",
     "MctInitialized",
