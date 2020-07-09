71b54139b8566ee7be3599568ecf2ca810ef53a3
vandy <chadvandewettering@gmail.com>
2020-07-08 20:24:04 -0400

    refactor campaign init

--- script/campaign/mod/mct_campaign_init.lua
+++ script/campaign/mod/mct_campaign_init.lua
@@ -1,7 +1,6 @@
 -- _init.lua files are solely for controlling the initialization functions. Everything else belongs in another file.
 local mct = get_mct()
 
-
 core:add_listener(
     "MCT_Init_SP",
     "LoadingGame",
@@ -9,10 +8,22 @@ core:add_listener(
     function(context)
         if not cm.game_interface:model():is_multiplayer() then
             ModLog("henk")
-            mct:init(context)
+            mct:load_and_start(context, false)
         else
+            -- save the host key into the save game
+            if cm:is_new_game() then
+                --local mp_file = loadfile("mct_mp")
+                --if mp_file then
+                    --local host_faction_key = mp_file().faction_key
+                    local host_faction_key = core:svr_load_string("mct_mp_host")
+            
+                    cm:set_saved_value("mct_host", host_faction_key)
+                --end
+            end
+
             ModLog("MP: ferk")
+            mct:load_and_start(context, true)
         end
-    end, 
+    end,
     true
 )
