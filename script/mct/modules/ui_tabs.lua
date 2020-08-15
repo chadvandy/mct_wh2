b33d472913fb619b42e919ddf29e462064567e7d
vandy <chadvandewettering@gmail.com>
2020-08-15 10:49:37 -0400

    ui_tabs start

---
+++ script/mct/modules/ui_tabs.lua
@@ -0,0 +1,9 @@
+--- This is the internal functionality to have fully customizable custom tabs for your mct_mod.
+
+local mct = mct
+
+local ui_tabs = {}
+
+function ui_tabs.new(mct_mod, key)
+        
+end
